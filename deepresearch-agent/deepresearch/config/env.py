d231f92180ffa221d9623b11efa0124747b3b25b
ananthanarayanan-28 <anantha.narayanan@oraczen.ai>
2025-09-09 12:09:03 +0530

    chore: gemini model and api

--- deepresearch-agent/deepresearch/config/env.py
+++ deepresearch-agent/deepresearch/config/env.py
@@ -6,6 +6,7 @@ load_dotenv()
 # llm
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
 
 # tools
--- deepresearch-agent/deepresearch/config/llm.py
+++ deepresearch-agent/deepresearch/config/llm.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 from langchain_openai.chat_models import ChatOpenAI
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
 
 from deepresearch.config.env import OPENAI_API_KEY
+from deepresearch.config.env import GOOGLE_API_KEY
 
 load_dotenv()
 
@@ -17,3 +19,27 @@ class LlmService:
             return llm
         except Exception:
             return None
+            
+    @classmethod
+    def get_openai_model(cls):
+        if not OPENAI_API_KEY:
+            raise ValueError("OPENAI API Key is not Set!")
+        try:
+            llm = ChatOpenAI(model="gpt-4o-mini",temperature=0.5)
+            return llm
+        
+        except Exception as Error:
+            return None 
+            
+            
+    @classmethod
+    def get_gemini_model(cls):
+        if not GOOGLE_API_KEY:
+            raise ValueError('GOOGLE API KEY is not Set!')
+        
+        try:
+            llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.5)
+            return llm
+        
+        except Exception as Error:
+            return None 
